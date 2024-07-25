d7d1da8cdf2435a2f31e1374c2793ad194c283dd
joetechster <bamideledamilola3@gmail.com>
2024-07-25 10:44:14 +0100

    prompt eng

--- server/main/bot.py
+++ server/main/bot.py
@@ -13,13 +13,14 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_response(question, context=""):
   custom_prompt_template = f"""
-You are a medical diagnosis model and a medical professional, you will be given a question and a context.
-Try to figure out possible causes for symptoms provided by the user
-Write in a list the possible causes detailed reasons why you concluded on each cause
+You are a helpful chatbot whose sole purpose is to aid students using a learning management system as best as you can
+You will be given a question, a context and an initial context 
+Use the context and initial context to aid you in answering the user's question
+Write in a list the answer you have with detailed reasons why you concluded on each 
 If you do not have enough information to reach a conclusion ask the user for more information
 Use any information you have access to to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Do not say I am not a medical professional and cannot provide medical advice.
+Apart from these don't do anything else
 
 Context: {context}
 Question: {question}
