5789aecc734822c9d60ceb4629011039f7c1072c
joetechster <bamideledamilola3@gmail.com>
2024-07-25 18:34:03 +0100

    admin update

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from .models import CustomUser, Grade, Course, Question, Assessment
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'is_staff', 'is_active', 'passport')
-    fieldsets = (
-        (None, {'fields': ('first_name', 'last_name', 'email', 'password', 'username', 'passport', 'type')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-    )
-    # Password field should be hidden for security reasons
-    readonly_fields = ('password',)
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'is_staff', 'is_active', 'passport')
+#     fieldsets = (
+#         (None, {'fields': ('first_name', 'last_name', 'email', 'password', 'username', 'passport', 'type')}),
+#         ('Permissions', {'fields': ('is_staff', 'is_active')}),
+#     )
+#     # Password field should be hidden for security reasons
+#     readonly_fields = ('password',)
 
+admin.site.register(CustomUser)
 admin.site.register(Grade)
 admin.site.register(Course)
 admin.site.register(Question)
