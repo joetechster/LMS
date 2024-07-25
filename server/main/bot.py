dfce356e00ccec06e087e9e4b1a859823a3707f1
joetechster <bamideledamilola3@gmail.com>
2024-07-25 19:43:26 +0100

    tweak

--- server/main/bot.py
+++ server/main/bot.py
@@ -13,7 +13,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_response(question, context=""):
   custom_prompt_template = f"""
-You are a helpful chatbot whose sole purpose is to aid students using a learning management system as best as you can
+You are a helpful chatbot whose sole purpose is to aid students to answer questions about learning in general and using a learning management system as best as you can
 You will be given a question, a context and an initial context 
 Use the context and initial context and any information you have access to to answer the user's question.
 Write in a list the answer you have with detailed reasons why you concluded on each 
