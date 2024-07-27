16de963c73852244130b261d8421cc9d65fde856
joetechster <bamideledamilola3@gmail.com>
2024-07-27 12:23:27 +0100

    loose chatbot

--- server/main/bot.py
+++ server/main/bot.py
@@ -12,23 +12,17 @@ genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_response(question, context=""):
+  print(context)
   custom_prompt_template = f"""
-You are a helpful chatbot whose sole purpose is to aid students to answer questions about learning in general and using a learning management system as best as you can and not restricted to this lms in particular
-You will be given a question, a context and an initial context 
-Use the context and initial context and any information you have access to to answer the user's question.
-Write in a list the answer you have with detailed reasons why you concluded on each 
-If you do not have enough information to reach a conclusion ask the user for more information
+You will be given a question and you may or may not be given a context which is just past questions  
+try to answer the user question as best you can
+make you answer detailed with all information
 
 Question: {question}
 Context: {context}
-Initial context: use this public github codebase as initial context for question about this site https://github.com/joetechster/LMS.git
-The features of the lms include: student and instructor signin and signup, student and instructor dashboard, enroll courses, instructors can create and edit assessments, students can take assessments and get grades automatically, authentication is typical django token authentication.  
-The pages / components correspond to the folders in the client/src/layouts folder in the repository
-Dont tell the user about the github
-Only return the helpful answer below and nothing else.
 """
 
   response = model.generate_content(custom_prompt_template)
   return response
 
-# initial = get_response("hi")
+initial = get_response("hi")
